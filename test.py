6fbd336896768aa49cddedf69d61adcabf03b955
Fernando Calixto <fernandocalixtodev@gmail.com>
2020-01-26 20:57:07 -0300

    Initial commit (importing numpy and learning the first functions)

---
+++ test.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+my_list = [[1,2,3],[4,5,6],[7,8,9]]
+
+arr = np.array(my_list) # Transforming the list into an array
+
+x = np.arange(0,485, 5) # Generating a new array populated
+
+y = np.linspace(0, 50, 10) # Generating a new array with evenly spaces and decimals
