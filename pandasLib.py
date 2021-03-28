1e6bf28c044e69fbdd52a52dce73a2187ccfcbe4
Fernando Calixto <fernandocalixtodev@gmail.com>
2021-03-28 13:52:20 -0300

    chore: creating initial user dataframe to start doing some tests and minor comment improvements

--- pandasLib.py
+++ pandasLib.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from numpy.random import randn
 
-# In the line below I need to pass: data, rows name, columns name.
+# In the line below I need to pass the following params: data, row names, column names.
 x = pd.DataFrame(randn(5,4), ['A', 'B', 'C', 'D', 'E'], ['First', 'Second', 'Third', 'Fourth'])
 
 print(x)
