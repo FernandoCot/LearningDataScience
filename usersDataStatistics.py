72008cdd4f58aa5b03387f9337568b3d7afc9334
Fernando Calixto <fernandocalixtodev@gmail.com>
2021-03-28 14:48:13 -0300

    chore: importing the "userDataFrame" from csv file, and choosing the necessary information to filter and classify

---
+++ usersDataStatistics.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# The dataframe below constains these columns: name, age, birthday, gender.
+userDataFrame = pd.read_csv('./RawFiles/UserMockedDataFrame.csv')
+print(userDataFrame)
+
+# amount_of_men = 
+# amount_of_women = 
+
+# amount_of_people_under_60_years = 
+# amount_of_people_above_or_equal_60_years = 
+
+# amount_of_users_with_a_specific_initial_letter = 
