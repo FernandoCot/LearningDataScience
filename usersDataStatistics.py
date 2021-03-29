c79f2cb48fa914b605b00c28c907393225c47689
Fernando Calixto <fernandocalixtodev@gmail.com>
2021-03-29 06:35:46 -0300

    chore: renaming csv file, adding EOL spaces, separating important statistics info

--- usersDataStatistics.py
+++ usersDataStatistics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # The dataframe below constains these columns: name, age, birthday, gender.
-userDataFrame = pd.read_csv('./RawFiles/UserMockedDataFrame.csv')
+userDataFrame = pd.read_csv('./RawFiles/UserList.csv')
 print(userDataFrame)
 
 # amount_of_men = 
@@ -10,4 +10,5 @@ print(userDataFrame)
 # amount_of_people_under_60_years = 
 # amount_of_people_above_or_equal_60_years = 
 
+# amount_of_users_born_in_a_specific_month = 
 # amount_of_users_with_a_specific_initial_letter = 
